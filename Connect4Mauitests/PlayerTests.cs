3435a06c49e8801ef90a89e1983d1230531aeac7
Thomas Milton <perso.milton@gmail.com>
2025-03-13 14:12:52 +0100

    ajout des premiers tests unitaires

---
+++ Connect4Mauitests/PlayerTests.cs
@@ -0,0 +1,33 @@
+﻿using Connect4Maui.Models;
+
+namespace Connect4Mauitests
+{
+    public class PlayersTests
+    {
+        [Fact]
+        public void Player1_ShouldHaveRedColor()
+        {
+            // Arrange
+            var player1 = new Player1();
+
+            // Act
+            var color = player1.Color;
+
+            // Assert
+            Assert.Equal(Colors.Red, color);
+        }
+
+        [Fact]
+        public void Player2_ShouldHaveYellowColor()
+        {
+            // Arrange
+            var player2 = new Player2();
+
+            // Act
+            var color = player2.Color;
+
+            // Assert
+            Assert.Equal(Colors.Yellow, color);
+        }
+    }
+}
